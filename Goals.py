44a4c54a5db1eb288423229f9470526905464bb9
PegasusAi <82119277+thePegasusai@users.noreply.github.com>
2022-12-29 20:14:26 -0800

    Create Goals.py

---
+++ Goals.py
@@ -0,0 +1,20 @@
+Here is an example of code that can be used to create a pose estimation model for identifying a goal in soccer:
+
+import torch
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+# Load a pre-trained model for classification and return only the features
+backbone = torchvision.models.mobilenet_v2(pretrained=True).features
+# FasterRCNN needs to know the number of
+# output channels in a backbone. For mobilenet_v2, it's 1280
+# so we need to add it here
+backbone.out_channels = 1280
+
+# Let's make the RPN generate 5 x 3 anchors per spatial
+# location, with 5 different sizes and 3 different aspect
+# ratios. We have a Tuple[Tuple[int]] because each feature
+# map could potentially have different sizes and
+# aspect ratios
+anchor_generator = torchvision.models.detection.rpn.anchor_generator.AnchorGenerator(
+    sizes=(
